d53c36aa5d68a1c41455539983ea15876ebb5183
Silvester Kresna <silvesterkresna@gmail.com>
2022-11-08 22:38:54 +0700

    create concatStr

--- src/main.c
+++ src/main.c
@@ -5,3 +5,19 @@
 int main() {
     
 }
+
+void concatStr(char *str1, char *str2, char *str3) {
+    int i = 0;
+    while (str1[i] != '\0') {
+        str3[i] = str1[i];
+        i++;
+    }
+    int j = 0;
+    while (str2[i] != '\0') {
+        str3[i] = str2[j];
+        i++;
+        j++;
+    }
+    str3[i] = '\0';
+
+}
