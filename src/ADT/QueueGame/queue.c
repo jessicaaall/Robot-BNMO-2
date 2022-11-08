6063db70478c2f0bb438dadb0da356c4bc602e34
NNawval <nalendronuarf@gmail.com>
2022-11-08 22:40:31 +0700

    nambah adt queue

---
+++ src/ADT/QueueGame/queue.c
@@ -0,0 +1,67 @@
+/* 
+NIM                 : 18221085
+Nama                : Nalendro N.U.A.R.F
+Tanggal             : 15 oktober 2022
+Topik praktikum     : Queue
+Deskripsi           : implementasi program queue.h
+*/
+
+#include <stdio.h>
+#include "queue.h"
+void CreateQueue(Queue *q){
+    IDX_HEAD(*q) = IDX_UNDEF;
+    IDX_TAIL(*q) = IDX_UNDEF;
+
+}
+
+boolean isEmpty(Queue q){
+    return (IDX_HEAD(q)==IDX_UNDEF)&&(IDX_TAIL(q)==IDX_UNDEF);
+}
+
+boolean isFull(Queue q){
+    return ((IDX_HEAD(q) == 0) && (IDX_TAIL(q) == CAPACITY - 1));   
+}
+
+int length(Queue q){
+    if (isEmpty(q)){
+        return 0;
+    }
+    else{
+    return (IDX_TAIL(q)+1);
+    }
+}
+
+void enqueue(Queue *q, ElType val){
+    if (isEmpty(*q)){
+        IDX_HEAD(*q)=0;
+        IDX_TAIL(*q)=0;
+        TAIL(*q)=val;
+    }
+    else {
+        IDX_TAIL(*q)++;
+        TAIL(*q)=val;
+    }}
+
+
+void dequeue(Queue *q, ElType *val){
+    int i;
+    if ((IDX_HEAD(*q)==0)&&(IDX_TAIL(*q)==0)){
+        (*val)=HEAD(*q);
+        IDX_HEAD(*q) = IDX_UNDEF;
+        IDX_TAIL(*q) = IDX_UNDEF;
+    }
+    else{
+    for (i=0;i<IDX_TAIL(*q);i++){
+        (*q).buffer[i] = (*q).buffer[i+1];
+        IDX_TAIL(*q)--;
+    }}
+}
+
+void displayQueueGame(Queue Q){
+    int i;
+    printf("Berikut adalah daftar antrian game-mu\n");
+    for (i=0;i<=IDX_TAIL(Q);i++){
+        printf("%d. %s\n", i+1, Q.buffer[i]);
+    }
+}
+
---
+++ src/ADT/QueueGame/queue.h
@@ -0,0 +1,63 @@
+/* File : queue.h */
+/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi statik */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "boolean.h"
+
+#define IDX_UNDEF -1
+#define CAPACITY 100
+
+/* Definisi elemen dan address */
+typedef char *ElType;
+typedef struct {
+	ElType buffer[CAPACITY]; 
+	int idxHead;
+	int idxTail;
+} Queue;
+
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika q adalah Queue, maka akses elemen : */
+#define IDX_HEAD(q) (q).idxHead
+#define IDX_TAIL(q) (q).idxTail
+#define     HEAD(q) (q).buffer[(q).idxHead]
+#define     TAIL(q) (q).buffer[(q).idxTail]
+
+/* *** Kreator *** */
+void CreateQueue(Queue *q);
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmpty(Queue q);
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFull(Queue q);
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+
+
+int length(Queue q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+
+/* *** Primitif Add/Delete *** */
+void enqueue(Queue *q, ElType val);
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" . */
+
+void dequeue(Queue *q, ElType *val);
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+        q mungkin kosong */
+
+/* *** Display Queue *** */
+void displayQueueGame(Queue q);
+
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong akan menulis antrian game*/
+#endif
