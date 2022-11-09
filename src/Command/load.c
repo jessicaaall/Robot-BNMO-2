f663b2c22e98992abcd3559677adbbb2e29460a0
Silvester Kresna <silvesterkresna@gmail.com>
2022-11-09 20:57:50 +0700

    ubah2

--- src/Command/load.c
+++ src/Command/load.c
@@ -6,8 +6,11 @@ void load(char * filename, Tab * loadsave) {
     /* Membuka .txt dan menyimpan isi ke Tab loadsave setiap baris
     I.S. : filename terdefinisi, Tab kosong
     F.S. : Tab loadsave terisi dengan .txt yang bernama filename */
+    char *dir = "../../savefile/";
+    char *fileDir;
+    concatStr(dir, filename, fileDir);
     int i = 0;
-    STARTKATA(filename);
+    STARTKATA(fileDir);
     while (!EndKata) {
         (*loadsave).TI[i] = CKata;
         (*loadsave).Neff++;
