3f85b99ad26865da80d24588511beedef7603410
Jessica <jessicalistiaraa@gmail.com>
2022-11-11 02:36:43 +0700

    update listgame

--- src/ListGame/listgame.c
+++ src/ListGame/listgame.c
@@ -3,9 +3,13 @@
 
 void LISTGAME(Tab T) {
     printf("Berikut adalah daftar game yang tersedia\n");
-    int i;
-    for (i = 0; i < NbElmtArray(T); i++){
-        printf("%d. %s\n", i+1, T.TI[i].TabWord);
+    int i, j;
+    for (i = 0; i < NbElmtArray(T); i++) {
+        printf("%d. ", i+1);
+        for (j = 0; j < T.TI[i].Length; j++) {
+            printf("%c", T.TI[i].TabWord[j]);
+        }
+        printf("\n");
     }
 }
 /* Mencetak daftar game ke layar */
