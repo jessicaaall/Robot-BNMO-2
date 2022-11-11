6916ec8f9967612cb714489e1e9de456fb08bd31
Jessica <jessicalistiaraa@gmail.com>
2022-11-11 13:24:08 +0700

    update playgame

--- src/PlayGame/playgame.c
+++ src/PlayGame/playgame.c
@@ -1,41 +1,33 @@
 #include "playgame.h"
+#include <stdio.h>
 
-boolean compareString(char *str1, char *str2) {
-    int i = 0;
-    while (str1[i] != '\0' && str2[i] != '\0') {
-        if (str1[i] != str2[i]) {
-            return false;
-        }
-        i++;
-    }
-    return true;
-}
-
-void PLAYGAME(Queue *Q){
-    /*
-    I.S. Daftar game terdefinisi
-    F.S. Game yang dipilih dimainkan
-    */
-    int i, skor;
-    ElType game;
-
-    if (!isEmpty(*Q)){
-        printf("Berikut adalah daftar Game-mu\n");
-        displayQueueGame(*Q);
+void PLAYGAME(Queue *queuegame) {
+    if (isEmpty(*queuegame)) {
+        printf("Daftar antrian game-mu kosong. Tidak ada game untuk dimainkan.\n");
+    } else {
+        printf("Berikut adalah daftar antrian game-mu\n");
+        displayQueueGame(*queuegame);
         printf("\n");
-        dequeue(Q, &game);
-        if (compareString(game, "RNG")){
+        
+        ElType game;
+        int skor;
+
+        dequeue(queuegame, &game);
+        if (IsStringSame(game, "RNG")){
             printf("Loading RNG ...\n");
             skor = RNGgame();
-        } else if (compareString(game, "Diner DASH")){
+        } else if (IsStringSame(game, "Diner DASH")){
             printf("Loading DINER DASH ...\n");
             DINNERDASH();
-        } else if (compareString(game, "DINOSAUR IN EARTH") || compareString(game, "RISEWOMAN") || compareString(game, "EIFFEL TOWER")){
+        } else if (IsStringSame(game, "DINOSAUR IN EARTH") || IsStringSame(game, "RISEWOMAN") || IsStringSame(game, "EIFFEL TOWER")){
             printf("Game %s masih dalam maintenance, belum dapat dimainkan.\nSilahkan pilih game lain.\n", game);
         } else {
             printf("Loading %s ...\n\n", game);
             skor = skorgame();
         }
-    }
-    
+    }      
 }
+/*
+I.S. Daftar antrian game terdefinisi
+F.S. Game pertama pada daftar antrian game dimainkan
+*/
--- src/PlayGame/playgame.h
+++ src/PlayGame/playgame.h
@@ -3,16 +3,17 @@
 #ifndef PLAYGAME_H
 #define PLAYGAME_H
 
+#include "../ADT/MesinKata/mesinkata.h"
 #include "../ADT/Queue/queue.h"
 #include "../RNGgame/rnggame.h"
 #include "../DinerDash/diner_dash.h"
 #include "../SkorGame/skorgame.h"
 #include "../boolean.h"
 
-void PLAYGAME(Queue *Q);
+void PLAYGAME(Queue *queuegame);
 /*
-I.S. Daftar game terdefinisi
-F.S. Game yang dipilih dimainkan
+I.S. Daftar antrian game terdefinisi
+F.S. Game pertama pada daftar antrian game dimainkan
 */
 
 #endif
