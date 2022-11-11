ada74485d573744393c559b135bb6d6921dfae84
Jessica <jessicalistiaraa@gmail.com>
2022-11-11 10:20:25 +0700

    update help

--- src/Help/help.c
+++ src/Help/help.c
@@ -3,14 +3,14 @@
 
 void HELP () {
     printf("\n============================== HELP ==============================\n\n");
-    printf("START        : Membaca file konfigurasi default\n");
-    printf("LOAD <file>  : Membaca file yang berisi daftar game\n");
-    printf("SAVE <file>  : Menyimpan state game pemain saat ini ke suatu file\n");
-    printf("CREATEGAME   : Menambahkan game baru pada daftar game\n");
-    printf("LISTGAME     : Menampilkan daftar game\n");
-    printf("DELETEGAME   : Menghapus game dari daftar game\n");
-    printf("QUEUEGAME    : Mendaftarkan permainan ke dalam daftar antrian game\n");
-    printf("PLAYGAME     : Memainkan game pertama pada antrian game\n");
-    printf("SKIPGAME <n> : Melewatkan permainan sebanyak n\n");
-    printf("QUIT         : Keluar dari program\n");
+    printf("START           : Membaca file konfigurasi default\n");
+    printf("LOAD <file>     : Membaca file yang berisi daftar game\n");
+    printf("SAVE <file>     : Menyimpan state game pemain saat ini ke suatu file\n");
+    printf("CREATE GAME     : Menambahkan game baru pada daftar game\n");
+    printf("LIST GAME       : Menampilkan daftar game\n");
+    printf("DELETE GAME     : Menghapus game dari daftar game\n");
+    printf("QUEUE GAME      : Mendaftarkan permainan ke dalam daftar antrian game\n");
+    printf("PLAY GAME       : Memainkan game pertama pada antrian game\n");
+    printf("SKIPGAME <n>    : Melewatkan permainan sebanyak n\n");
+    printf("QUIT            : Keluar dari program\n");
 }
